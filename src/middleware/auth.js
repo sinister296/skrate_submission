43ac6fc2108653e57d1b79c1d86f07ed2d27ee88
sinister296 <naman.6.1998@gmail.com>
2022-08-08 20:30:47 +0530

    Fixing mddleware/auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,8 +4,8 @@ const User = require('../models/user')
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const user = await User.findOne({_id: decoded._id, 'tokens.token': token})
+        const decoded = jwt.verify(token, "skrateusertoken")
+        const user = await User.findOne({_id: decoded._id, 'token': token, 'role': "admin"})
 
         if(!user) {
             throw new Error()
